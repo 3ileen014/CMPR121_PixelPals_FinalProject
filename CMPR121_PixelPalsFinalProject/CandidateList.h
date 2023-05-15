55cd5b62bcda7a04dd47e364180ffe6bcb5f45f2
3ileen014 <3ileen.k0124@gmail.com>
2023-05-15 16:39:28 -0700

    comment

--- CMPR121_PixelPalsFinalProject/CandidateList.h
+++ CMPR121_PixelPalsFinalProject/CandidateList.h
@@ -1,1 +1,18 @@
 #pragma once
+#ifndef CANDIDATELIST_H
+#define CANDIDATELIST_H
+
+#include "Node.h"
+
+class CandidateList {
+private:
+	Node* first;
+	Node* last;
+	int count;
+public:
+	CandidateList();
+};
+
+//edit
+
+#endif

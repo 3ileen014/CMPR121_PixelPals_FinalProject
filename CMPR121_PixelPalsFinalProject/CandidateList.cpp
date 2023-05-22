a4594c8831397bb5ddc3667d85610fa89e8aecc2
3ileen014 <3ileen.k0124@gmail.com>
2023-05-22 14:10:31 -0700

    attempted addCandidate()

--- CMPR121_PixelPalsFinalProject/CandidateList.cpp
+++ CMPR121_PixelPalsFinalProject/CandidateList.cpp
@@ -2,8 +2,23 @@
 #include <string>
 #include <iostream>
 
-CandidateList::CandidateList() {
+CandidateList::CandidateList() 
+{
 	first = nullptr;
 	last = nullptr;
 	count = 0;
+}
+
+void CandidateList::addCandidate(const CandidateType newCand) 
+{
+	if (count == 0) {
+		first = new Node();
+		first->setCandidate(newCand);
+		last = first;
+	}
+	else {
+		last = new Node();
+		last->setCandidate(newCand);
+	}
+	count++;
 }
